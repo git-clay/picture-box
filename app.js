3d5cb4f2efa3a507a9321b92b9187e7c8bbd1849
git-clay <ce.smith5525@gmail.com>
2017-01-20 12:48:00 -0700

    large image resizing work

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var mainImg= document.getElementsByClassName('mainImg')[0],
 	tImgs = document.getElementsByClassName('img'),
-	len = tImgs.length;
-console.log(mainImg)
+	len = tImgs.length,
+	src= tImgs[0].getAttribute('src'),
+	append = mainImg.innerHTML ='<img class="largeImg" src="'+src+'">';
+console.log(src,append)
 
 for(var i = 0;i<len;i++){
 	tImgs[i].addEventListener('click',function(){
