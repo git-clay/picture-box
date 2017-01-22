f15c5cbd6004f4ef0d9ece5963a5ec502d02bf59
git-clay <ce.smith5525@gmail.com>
2017-01-22 13:28:41 -0700

    countertop specific changed js and added imgs

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 var mainImg= document.getElementsByClassName('mainImg')[0],
 	tImgs = document.getElementsByClassName('img'),
 	len = tImgs.length,
-	src= tImgs[0].getAttribute('src'),
-	append = mainImg.innerHTML ='<img class="largeImg" src="'+src+'">';
+	lrgImgArr = ['bevel-bullnose','bevel','bullnose','classic-ogee','double-bevel',
+	'double-ogee','double-radius','half-bullnose','step-half-bullnose',
+	'eased','dupont-bullnose','triple-pencil','waterfall'],
+	src= tImgs[0].getAttribute('src');
+
+	function append (newimg){ 
+		mainImg.innerHTML ='<img class="largeImg" src="./img/visualizer/'+newimg+'-countertop.jpg">'};
+
+
 
 for(var i = 0;i<len;i++){
+	var cur =lrgImgArr[i];
 	tImgs[i].addEventListener('mouseover',function(){
 		this.style.opacity = '.56'
 	})
 	tImgs[i].addEventListener('mouseout',function(){
 		this.style.opacity = '1'
 	})
-
 	tImgs[i].addEventListener('click',function(){
-	var cur=this.getAttribute('src'),
-		append = mainImg.innerHTML ='<img class="largeImg" src="'+cur+'">'
+		var cur=this.getAttribute('id');
+		console.log(cur)
+		append(lrgImgArr[cur])
 	})
 }
+	// var cur=this.getAttribute('src'),
+	// 	append = mainImg.innerHTML ='<img class="largeImg" src="'+cur+'">'
+	
